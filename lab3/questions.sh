dd2b01302ea0103ea9abc8258963f568dbd6cf30
s3540645 <s3540645@student.rmit.edu.au>
2020-08-14 08:19:05 -0700

    fix

---
+++ lab3/questions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# script used for running sudo in pycharm venv
+# printing out arguements
+count=1
+arguements=""
+filename=questions.py
+for para in $@:
+do
+ echo arguement $count is $para
+ arguements="${arguements} ${para}"
+ count=$((count+1))
+done
+#for fun
+echo arguements $arguements
+#run the python script as sudo
+sudo  /home/alex/PycharmProjects/SDN/lab3/venv/bin/python $filename $*
